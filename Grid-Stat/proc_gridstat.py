1e1942f6dd84538e5d0a67835fd69a9a832d2c97
cgrudz <cgrudzien@comet-ln3.sdsc.edu>
2023-06-27 12:23:06 -0700

    Rewriting proc_gridstat for better logging, single date outputs, and parallelisation over start dates

--- Grid-Stat/proc_gridstat.py
+++ Grid-Stat/proc_gridstat.py
@@ -89,37 +89,72 @@ OUT_ROOT = '/cw3e/mead/projects/cwp106/scratch/' + CSE
 # standard string indentation
 STR_INDT = '    '
 
+# convert to date times
+if len(STRT_DT) != 10:
+    print('ERROR: STRT_DT, ' + STRT_DT + ', is not in YYYYMMDDHH format.')
+    sys.exit(1)
+else:
+    s_iso = STRT_DT[:4] + '-' + STRT_DT[4:6] + '-' + STRT_DT[6:8] +\
+            '_' + STRT_DT[8:]
+    strt_dt = dt.fromisoformat(s_iso)
+
+if len(END_DT) != 10:
+    print('ERROR: END_DT, ' + END_DT +\
+            ', is not in YYYYMMDDHH format.')
+    sys.exit(1)
+else:
+    e_iso = END_DT[:4] + '-' + END_DT[4:6] + '-' + END_DT[6:8] +\
+            '_' + END_DT[8:]
+    end_dt = dt.fromisoformat(e_iso)
+
+if len(CYC_INT) != 2:
+    print('ERROR: CYC_INT, ' + CYC_INT + ', is not in HH format.')
+    sys.exit(1)
+else:
+    cyc_int = CYC_INT + 'H'
+
 # container for map
 CNFGS = []
 
-for CTR_FLW in CTR_FLWS:
-    for GRD in GRDS:
-        for PRFX in PRFXS:
-            # storage for configuration settings as arguments of proc_gridstat
-            # the function definition and role of these arguments are in the
-            # next section directly below
-            CNFG = []
+# generate the date range for the analyses
+analyses = pd.date_range(start=strt_dt, end=end_dt, freq=cyc_int).to_pydatetime()
 
-            # control flow / directory name
-            CNFG.append(CTR_FLW)
+print('Processing configurations:')
+for anl_dt in analyses:
+    anl_strng = anl_dt.strftime('%Y%m%d%H')
+    for CTR_FLW in CTR_FLWS:
+        for GRD in GRDS:
+            for PRFX in PRFXS:
+                print(STR_INDT + anl_strng + ' ' + PRFX + ' ' + CTR_FLW +\
+                      ' ' + GRD)
+                # storage for configuration settings as arguments of proc_gridstat
+                # the function definition and role of these arguments are in the
+                # next section directly below
+                CNFG = []
 
-            # prefix for gridstat outputs
-            CNFG.append(PRFX)
-
-            # grid to be processed
-            CNFG.append(GRD)
-
-            # path to cycle directories from IN_ROOT
-            CNFG.append('/' + CTR_FLW)
-            
-            # path to gridstat outputs from cycle directory
-            CNFG.append('/' + GRD)
-
-            # path to pandas output directories from OUT_ROOT
-            CNFG.append('/' + CTR_FLW)
-
-            # append configuration to be mapped
-            CNFGS.append(CNFG)
+                # forecast zero hour date time
+                CNFG.append(anl_strng)
+    
+                # control flow / directory name
+                CNFG.append(CTR_FLW)
+    
+                # prefix for gridstat outputs
+                CNFG.append(PRFX)
+    
+                # grid to be processed
+                CNFG.append(GRD)
+    
+                # path to cycle directories from IN_ROOT
+                CNFG.append('/' + CTR_FLW)
+                
+                # path to gridstat outputs from cycle directory
+                CNFG.append('/' + GRD)
+    
+                # path to pandas output directories from OUT_ROOT
+                CNFG.append('/' + CTR_FLW)
+    
+                # append configuration to be mapped
+                CNFGS.append(CNFG)
 
 ##################################################################################
 # Process data routine
@@ -127,143 +162,119 @@ for CTR_FLW in CTR_FLWS:
 #  function for multiprocessing parameter map
 def proc_gridstat(cnfg):
     # unpack argument list
-    ctr_flw, prfx, grd, in_cyc_dir, in_dt_subdir, out_cyc_dir = cnfg
+    anl_strng, ctr_flw, prfx, grd, in_cyc_dir, in_dt_subdir, out_cyc_dir = cnfg
 
     # include underscore if prefix is of nonzero length
     if len(prfx) > 0:
         prfx =+ '_'
 
-    # define derived data paths 
-    in_data_root = IN_ROOT + in_cyc_dir 
+    log_dir = OUT_ROOT + '/batch_logs'
+    os.system('mkdir -p ' + log_dir)
 
-    out_data_root = OUT_ROOT + '/' + out_cyc_dir
-    os.system('mkdir -p ' + out_data_root)
-    out_path = out_data_root + '/grid_stats_' + prfx + grd + '_' + STRT_DT +\
-               '_to_' + END_DT + '.bin'
-    
-    with open(out_data_root + '/proc_gridstat_' + prfx + ctr_flw + '_' + grd +\
-              '_log.txt', 'w') as log_f:
-        # check for input root directory
+    with open(log_dir + '/proc_gridstat_' + prfx + ctr_flw + '_' + grd +\
+              '_' + anl_strng + '.log', 'w') as log_f:
+
+        # define derived data paths 
+        in_data_root = IN_ROOT + in_cyc_dir 
+
+        out_data_root = OUT_ROOT + out_cyc_dir
+        os.system('mkdir -p ' + out_data_root)
+        
+        # check for input / output root directory
         if not os.path.isdir(in_data_root):
             print('ERROR: input data root directory ' + in_data_root +\
                     ' does not exist.', file=log_f)
             sys.exit(1)
         
-        # convert to date times
-        if len(STRT_DT) != 10:
-            print('ERROR: STRT_DT, ' + STRT_DT +\
-                    ', is not in YYYYMMDDHH format.', file=log_f)
+        # check for input / output root directory
+        elif not os.path.isdir(out_data_root):
+            print('ERROR: output data root directory ' + out_data_root +\
+                    ' does not exist.', file=log_f)
             sys.exit(1)
-        else:
-            s_iso = STRT_DT[:4] + '-' + STRT_DT[4:6] + '-' + STRT_DT[6:8] +\
-                    '_' + STRT_DT[8:]
-            strt_dt = dt.fromisoformat(s_iso)
-
-        if len(END_DT) != 10:
-            print('ERROR: END_DT, ' + END_DT +\
-                    ', is not in YYYYMMDDHH format.', file=log_f)
-            sys.exit(1)
-        else:
-            e_iso = END_DT[:4] + '-' + END_DT[4:6] + '-' + END_DT[6:8] +\
-                    '_' + END_DT[8:]
-            end_dt = dt.fromisoformat(e_iso)
-
-        if len(CYC_INT) != 2:
-            print('ERROR: CYC_INT, ' + CYC_INT +\
-                    ', is not in HH format.', file=log_f)
-            sys.exit(1)
-        else:
-            cyc_int = CYC_INT + 'H'
-        
-        # generate the date range for the analyses
-        analyses = pd.date_range(start=strt_dt, end=end_dt,
-                                 freq=cyc_int).to_pydatetime()
         
         # initiate empty dictionary for storage of dataframes by keyname
         data_dict = {}
-        
-        print('Processing dates ' + STRT_DT + ' to ' + END_DT, file=log_f)
-        for anl_dt in analyses:
-            # directory string format
-            anl_strng = anl_dt.strftime('%Y%m%d%H')
-            
-            # define the gridstat files to open based on the analysis date
-            in_paths = in_data_root + '/' + anl_strng + in_dt_subdir  +\
-                       '/grid_stat_' + prfx + '*.txt'
-        
-            # loop sorted grid_stat_prfx* files, sorting compares first on the
-            # length of lead time for non left-padded values
-            in_paths = sorted(glob.glob(in_paths),
-                              key=lambda x:(len(x.split('_')[-4]), x))
-            for in_path in in_paths:
-                print(STR_INDT + 'Opening file ' + in_path, file=log_f)
-        
-                # cut the diagnostic type from file name
-                fname = in_path.split('/')[-1]
-                split_name = fname.split('_')
-                postfix = split_name[-1].split('.')
-                postfix = postfix[0]
-        
-                # open file, load column names, then loop lines
-                with open(in_path) as f:
-                    cols = f.readline()
-                    cols = cols.split()
+    
+        # define the gridstat files to open based on the analysis date
+        in_paths = in_data_root + '/' + anl_strng + in_dt_subdir  +\
+                   '/grid_stat_' + prfx + '*.txt'
+    
+        # define the output binary file for pickled dataframe per date
+        out_path = out_data_root + '/' + anl_strng + '/grid_stats_' + prfx +\
+                   grd + '_' + anl_strng + '.bin'
+        # loop sorted grid_stat_prfx* files, sorting compares first on the
+        # length of lead time for non left-padded values
+        in_paths = sorted(glob.glob(in_paths),
+                          key=lambda x:(len(x.split('_')[-4]), x))
+        for in_path in in_paths:
+            print('Opening file ' + in_path, file=log_f)
+    
+            # cut the diagnostic type from file name
+            fname = in_path.split('/')[-1]
+            split_name = fname.split('_')
+            postfix = split_name[-1].split('.')
+            postfix = postfix[0]
+    
+            # open file, load column names, then loop lines
+            with open(in_path) as f:
+                cols = f.readline()
+                cols = cols.split()
+                
+                if len(cols) > 0:
+                    fname_df = {} 
+                    tmp_dict = {}
+                    df_indx = 1
+    
+                    print(STR_INDT + 'Loading columns:', file=log_f)
+                    for col_name in cols:
+                        print(STR_INDT * 2 + col_name, file=log_f)
+                        fname_df[col_name] = [] 
+    
+                    fname_df =  pd.DataFrame.from_dict(fname_df,
+                                                       orient='columns')
+    
+                    # parse file by line, concatenating columns
+                    for line in f:
+                        split_line = line.split()
+    
+                        for i in range(len(split_line)):
+                            val = split_line[i]
+    
+                            # filter NA vals
+                            if val == 'NA':
+                                val = np.nan
+                            tmp_dict[cols[i]] = val
+    
+                        tmp_dict['line'] = [df_indx]
+                        tmp_dict = pd.DataFrame.from_dict(tmp_dict,
+                                                          orient='columns')
+                        fname_df = pd.concat([fname_df, tmp_dict], axis=0)
+                        df_indx += 1
+    
+                    fname_df['line'] = fname_df['line'].astype(int)
                     
-                    if len(cols) > 0:
-                        fname_df = {} 
-                        tmp_dict = {}
-                        df_indx = 1
-        
-                        print(STR_INDT + 'Loading columns:', file=log_f)
-                        for col_name in cols:
-                            print(STR_INDT * 2 + col_name, file=log_f)
-                            fname_df[col_name] = [] 
-        
-                        fname_df =  pd.DataFrame.from_dict(fname_df,
-                                                           orient='columns')
-        
-                        # parse file by line, concatenating columns
-                        for line in f:
-                            split_line = line.split()
-        
-                            for i in range(len(split_line)):
-                                val = split_line[i]
-        
-                                # filter NA vals
-                                if val == 'NA':
-                                    val = np.nan
-                                tmp_dict[cols[i]] = val
-        
-                            tmp_dict['line'] = [df_indx]
-                            tmp_dict = pd.DataFrame.from_dict(tmp_dict,
-                                                              orient='columns')
-                            fname_df = pd.concat([fname_df, tmp_dict], axis=0)
-                            df_indx += 1
-        
-                        fname_df['line'] = fname_df['line'].astype(int)
-                        
-                        if postfix in data_dict.keys():
-                            last_indx = data_dict[postfix].index[-1]
-                            fname_df['line'] = fname_df['line'].add(last_indx)
-                            fname_df = fname_df.set_index('line')
-                            data_dict[postfix] = pd.concat([data_dict[postfix],
-                                                            fname_df], axis=0)
-        
-                        else:
-                            fname_df = fname_df.set_index('line')
-                            data_dict[postfix] = fname_df
-        
+                    if postfix in data_dict.keys():
+                        last_indx = data_dict[postfix].index[-1]
+                        fname_df['line'] = fname_df['line'].add(last_indx)
+                        fname_df = fname_df.set_index('line')
+                        data_dict[postfix] = pd.concat([data_dict[postfix],
+                                                        fname_df], axis=0)
+    
                     else:
-                        print('WARNING: file ' + in_path +\
-                                ' is empty, skipping this file.', file=log_f)
-        
-                    print(STR_INDT + 'Closing file ' + in_path, file=log_f)
-        
+                        fname_df = fname_df.set_index('line')
+                        data_dict[postfix] = fname_df
+    
+                else:
+                    print('WARNING: file ' + in_path +\
+                            ' is empty, skipping this file.', file=log_f)
+    
+                print('Closing file ' + in_path, file=log_f)
+    
         print('Writing out data to ' + out_path, file=log_f)
         with open(out_path, 'wb') as f:
             pickle.dump(data_dict, f)
 
-        return 'Completed: ' + prfx + ctr_flw + ' ' + grd + '\n'
+        print('Completed: ' + anl_strng + ' ' + prfx + ctr_flw + ' ' + grd) 
 
 ##################################################################################
 # Runs multiprocessing on parameter grid

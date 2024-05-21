b7f9de036d20d0b3bf42aa83b665b7e89bacec6f
sawyer-haase-lab <153561150+sawyer-haase-lab@users.noreply.github.com>
2024-05-21 14:02:14 -0700

    Update config_preprocessMPAS.sh

--- preprocessMPAS/config_preprocessMPAS.sh
+++ preprocessMPAS/config_preprocessMPAS.sh
@@ -12,6 +12,8 @@
 # Source HPC parameters
 source ../config_MET-tools.sh
 
+MESH_IN_DIR  = "/glade/derecho/scratch/ersawyer/sio-cw3e/JEDI-MPAS-Common-Case/SIMULATION_IO/2023021818_cycle_start"
+
 # Define the case-wise sub-directory for path names with case-study nesting,
 # leave as empty string "" if not needed
 export CSE=2023021818_cycle_start

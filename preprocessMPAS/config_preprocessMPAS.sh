4bc588d0db56e5483737542f25bbb308414b9b58
sawyer-haase-lab <153561150+sawyer-haase-lab@users.noreply.github.com>
2024-05-28 11:21:51 -0700

    Update config_preprocessMPAS.sh
    Define the variable MSH_ROOT

--- preprocessMPAS/config_preprocessMPAS.sh
+++ preprocessMPAS/config_preprocessMPAS.sh
@@ -12,6 +12,9 @@
 # Source HPC parameters
 source ../config_MET-tools.sh
 
+#Define where the mesh data is stored. Set to '' if all output files contain mesh data.
+export MSH_ROOT=''
+
 # Define the case-wise sub-directory for path names with case-study nesting,
 # leave as empty string "" if not needed
 export CSE=2022122800_valid_date

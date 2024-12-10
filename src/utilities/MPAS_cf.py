299f7ae989a799f07b24f9e3a2c9a9721913af38
Colin James Grudzien <cgrudzien@ucsd.edu>
2024-12-10 10:53:26 -0800

    Uploading IVT codes sketched by Corrine

--- src/utilities/MPAS_cf.py
+++ src/utilities/MPAS_cf.py
@@ -13,17 +13,11 @@ from utilities import *
 # Utility Methods
 ##################################################################################
 
-def cf_precip(ds_in, f_time=None):
-    """Computes total precip from the dataset in and returns a dataset out.
-
-    Total precip is computed from rainc + rainnc, with attributes copied from
-    file, with new description and standard / long name added as attributes.
-    Returns dataset with dimensions and coordinates from the original.
+def global_attrs(ds_in, ds_out, f_time):
+    """
+    Function to set global xarray data attributes.
     """
 
-    # unpack grid- / convective-scale precip
-    precip_g = ds_in.rainnc
-    precip_c = ds_in.rainc
     if f_time == None:
         valid_dt = ds_in.xtime.values[0]
         start_dt = ds_in.config_start_time
@@ -32,8 +26,7 @@ def cf_precip(ds_in, f_time=None):
         ds_time = xr.open_dataset(f_time)
         valid_dt = ds_time.xtime.values[0]
         start_dt = ds_time.config_start_time
-
-    precip = precip_g + precip_c
+        ds_time.close()
 
     # we write out start / valid times in iso / unix for MET attributes
     unix_dt = dt(1970, 1, 1)
@@ -48,42 +41,34 @@ def cf_precip(ds_in, f_time=None):
     # accumulations are computed from simulation inialization time
     accu_sec = valid_nx - start_nx
 
-    # infer grid dimensions
-    nlat = len(ds_in.latitude.values)
-    nlon = len(ds_in.longitude.values)
+    # Fixes time dim and adds forecast_reference_time var
+    ds_out['time'] = np.array([valid_nx])
+    ds_out['forecast_reference_time'] = \
+            xr.DataArray(np.array([start_nx]), dims=['time'],
+                    coords=dict(time=ds_out['time']))
 
-    ds_out = xr.Dataset(
-            data_vars=dict(
-                precip=(['time', 'lat', 'lon'], np.reshape(precip.values,
-                    [1, nlat, nlon])
-                    ),
-                forecast_reference_time=(['time'], np.array([start_nx])),
-                ),
-            coords=dict(
-                time=(['time'], np.array([valid_nx])),
-                lat=('lat', ds_in.latitude.values),
-                lon=('lon', ds_in.longitude.values),
-                ),
-            )
-
+    # Declares fill value
     fill_val = 1e20
     ds_out.fillna(fill_val)
 
-    ds_out.attrs = {'Conventions': 'CF-1.6'}
+    # For data vars, who share these attributes
+    repeat_attrs = {
+            'missing_value':fill_val, 
+            'valid_time':valid_is, 
+            'valid_time_ut':valid_nx, 
+            'init_time':start_is, 
+            'init_time_ut':start_nx,
+            'accum_time_sec': accu_sec,
+            }
 
-    ds_out.precip.attrs = {
-        'description': 'Sum of grid- / convective-scale precipitation',
-        'standard_name': 'total_precipitation_amount',
-        'long_name': 'Accumulated Total Precipitation Over Simulation',
-        'units': 'mm',
-        'missing_value': fill_val,
-        'valid_time': valid_is,
-        'valid_time_ut': valid_nx,
-        'init_time': start_is,
-        'init_time_ut': start_nx,
-        'accum_time_sec': accu_sec,
-        }
+    # Global DS attribute
+    ds_out.attrs = {
+            'Conventions':'CF-1.6', 
+            'notes':'Created with MET-Tools', 
+            'institution':'CW3E - Scripps Institution of Oceanography',
+            }
 
+    # Global ds dimension and coordinate attributes
     ds_out.time.attrs = {
         'long_name': 'Time',
         'standard_name': 'time',
@@ -92,23 +77,23 @@ def cf_precip(ds_in, f_time=None):
         'calendar': 'standard',
         'axis': 'T',
         }
-
+    
     ds_out.lat.attrs = {
-        'long_name': 'latitude',
+        'long_name': 'Latitude',
         'standard_name': 'latitude',
         'units': 'degrees_north',
         'missing_value': fill_val,
         'axis': 'Y',
         }
-
+    
     ds_out.lon.attrs = {
-        'long_name': 'longitude',
+        'long_name': 'Longitude',
         'standard_name': 'longitude',
         'units': 'degrees_east',
         'missing_value': fill_val,
-        'axis': 'X',
+        'axis':'X',
         }
-
+    
     ds_out.forecast_reference_time.attrs = {
         'long_name': 'Forecast Reference Time',
         'standard_name': 'forecast_reference_time',
@@ -118,13 +103,104 @@ def cf_precip(ds_in, f_time=None):
         'units': 'seconds since 1970-01-01T00:00:00',
         'description': 'Simulation initial time',
         }
+    
+    return ds_out, repeat_attrs
+
+def prep_attrs(ds_in, ds_out, f_time=None):
+    """
+    Function to assign attributes to new data variables.
+    """
+
+    # Sets up global dataset attributes
+    ds_out, repeat_attrs = global_attrs(ds_in, ds_out, f_time)
+
+    if 'precip' in ds_out.data_vars:
+        ds_out.precip.attrs = {
+                'description': 'Sum of grid- / convective-scale precipitation',
+                'standard_name': 'total_precipitation_amount',
+                'long_name': 'Accumulated Total Precipitation Over Simulation',
+                'units': 'mm',
+                **repeat_attrs
+                }
+
+    if 'IWV' in ds_out.data_vars:
+        ds_out.IWV.attrs = {
+                'description':'Column-integrated water vapor',
+                'standard_name':'integrated_water_vapor',
+                'long_name':'Integrated Water Vapor',
+                'units':'kg m-2',
+                **repeat_attrs
+                }
+
+    if 'IVT' in ds_out.data_vars:
+        ds_out.IVT.attrs = {
+                'description':'Column-integrated vapor transport',
+                'standard_name':'integrated_vapor_transport',
+                'long_name':'Integrated Vapor Transport',
+                'units':'kg m-1 s-1',
+                **repeat_attrs
+                }
+
+    if 'IVTU' in ds_out.data_vars:
+        ds_out.IVTU.attrs = {
+                'description':'Column-integrated vapor transport u-component',
+                'standard_name':'integrated_vapor_transport_u',
+                'long_name':'Integrated Vapor Transport U-component',
+                'units':'kg m-1 s-1',
+                **repeat_attrs
+                }
+
+    if 'IVTV' in ds_out.data_vars:
+        ds_out.IVTV.attrs = {
+                'description':'Column-integrated vapor transport v-component',
+                'standard_name':'integrated_vapor_transport_v',
+                'long_name':'Integrated Vapor Transport V-component',
+                'units':'kg m-1 s-1',
+                **repeat_attrs
+                }
+
+    return ds_out
+
+def cf_precip(ds_in, f_time=None):
+    """Computes total precip from the dataset in and returns a dataset out.
+
+    Total precip is computed from rainc + rainnc, with attributes copied from
+    file, with new description and standard / long name added as attributes.
+    Returns dataset with dimensions and coordinates from the original.
+    """
+
+    # unpack grid- / convective-scale precip
+    precip_g = ds_in.rainnc
+    precip_c = ds_in.rainc
+    precip = precip_g + precip_c
+
+    # infer grid dimensions
+    nlat = len(ds_in.latitude.values)
+    nlon = len(ds_in.longitude.values)
+
+    ds_out = xr.Dataset(
+            data_vars=dict(
+                precip=(['time', 'lat', 'lon'],
+                    np.reshape(precip.values, [1, nlat, nlon])
+                    ),
+                ),
+            coords=dict(
+                time=(['time'], np.array([0])),
+                lat=('lat', ds_in.latitude.values),
+                lon=('lon', ds_in.longitude.values),
+                ),
+            )
+
+    # Assigns attributes
+    ds_out = prep_attrs(ds_in, ds_out, f_time)
 
     return ds_out
 
 def cf_precip_bkt(ds_curr, ds_prev):
     """Computes accumulated precip between two model times from time series.
 
-    Inputs are precip data arrays at
+    Inputs are precip data arrays at current and previous time over which to
+    compute the accumulation bucket.
     """
     ds_out = ds_curr.rename_vars({'precip': 'precip_bkt'})
     ds_out.precip_bkt.values = ds_curr.precip.values - ds_prev.precip.values
@@ -150,6 +226,65 @@ def cf_precip_bkt(ds_curr, ds_prev):
         'init_time_ut': prev_nx,
         'accum_time_sec': accu_sec,
         }
+
+    return ds_out
+
+def cf_ivt(ds_in, f_time=None):
+    """
+    Function to calculate IVT and IWV and put into cf-compliant xarray dataset.
+    """
+
+    # infer grid dimensions
+    nlat = len(ds_in.latitude.values)
+    nlon = len(ds_in.longitude.values)
+
+    # Limits IVT calc from surface to ~200 mb
+    pres = ds_in['pressure']
+    top_idx = np.min(np.argwhere(pres.values <= 2500.0)[:, 1])
+    pres = pres.isel(nVertLevels=slice(0, top_idx))
+
+    # water vapor mixing ratio [kg/kg]
+    qv = ds_in['qv'].isel(nVertLevels=slice(0, top_idx-1))
+   
+    # u component of the wind [m/s]
+    u = ds_in['uReconstructZonal'].isel(nVertLevels=slice(0, top_idx-1))
+    
+    # v component of the wind [m/s]
+    v = ds_in['uReconstructMeridional'].isel(nVertLevels=slice(0, top_idx-1))
+
+    # Vertical Pa differences between layer interfaces
+    d_pres = pres.diff(dim='nVertLevels')
+
+    # Calculates IWV
+    IWV =((qv * d_pres)/9.81).sum(dim='nVertLevels') 
+
+    # Calculates u and v components of IVT
+    IVTU = ((qv * d_pres * u)/9.81).sum(dim='nVertLevels')
+    IVTV = ((qv * d_pres * v)/9.81).sum(dim='nVertLevels')
+
+    # Combines components into full IVT values
+    IVT = np.sqrt((IVTV**2)+(IVTU**2))
+
+    # Prepares output ds
+    ds_out = xr.Dataset(
+            data_vars = dict(
+                IWV=(['time', 'lat', 'lon'],
+                    np.reshape(IWV.values, [1, nlat, nlon])),
+                IVT=(['time', 'lat', 'lon'],
+                    np.reshape(IVT.values, [1, nlat, nlon])),
+                IVTU=(['time', 'lat', 'lon'],
+                    np.reshape(IVTU.values, [1, nlat, nlon])),
+                IVTV=(['time', 'lat', 'lon'],
+                    np.reshape(IVTU.values, [1, nlat, nlon]))),
+            coords = dict(
+                time = (['time'], np.array([0])),
+                lat = ('lat', ds_in.latitude.values),
+                lon = ('lon', ds_in.longitude.values),
+                )
+            )
+
+    # Assigns attributes
+    ds_out = prep_attrs(ds_in, ds_out, f_time)
 
     return ds_out
 
--- src/utilities/WRF_cf.py
+++ src/utilities/WRF_cf.py
@@ -31,6 +31,191 @@ LON2=272.
 # Utility Methods
 ##################################################################################
 
+def unstagger(var, stagger_dim):
+    """
+    Function from WRF-python to unstagger variables.
+    """
+    
+    var_shape = var.shape
+    num_dims = var.ndim
+    stagger_dim_size = var_shape[stagger_dim]
+    
+    full_slice = slice(None)
+    slice1 = slice(0, stagger_dim_size - 1, 1)
+    slice2 = slice(1, stagger_dim_size, 1)
+    
+    # default to full slices
+    dim_ranges_1 = [full_slice] * num_dims
+    dim_ranges_2 = [full_slice] * num_dims
+    
+    # for the stagger dim, insert the appropriate slice range
+    dim_ranges_1[stagger_dim] = slice1
+    dim_ranges_2[stagger_dim] = slice2
+    
+    result = 0.5*(var[tuple(dim_ranges_1)] + var[tuple(dim_ranges_2)])
+
+    return result
+
+def unstagger_uv(u, v):
+    """
+    Function to calculate unstaggered u and v wind components.
+    """
+
+    # Extracts attributes to repopulate later
+    u_attrs = u.attrs
+    v_attrs = v.attrs
+
+    unstag_u = unstagger(u, -1)
+    unstag_v = unstagger(v, -2)
+
+    # Fixes metadata
+    unstag_u = unstag_u.swap_dims({'west_east_stag':'west_east'})
+    unstag_v = unstag_v.swap_dims({'south_north_stag':'south_north'})
+
+    unstag_u.attrs = u_attrs
+    del unstag_u.attrs['stagger']
+
+    unstag_v.attrs = v_attrs
+    del unstag_v.attrs['stagger']
+
+    return unstag_u, unstag_v
+
+def global_attrs(ds_in, ds_out, init_offset=0):
+    """
+    Function to set global xarray data attributes.
+    """
+    
+    # Time vars
+    valid_dt = ds_in.XTIME.values[0]
+    start_dt = ds_in.START_DATE
+
+    # we write out start / valid times in iso / unix for MET attributes
+    unix_dt = dt(1970, 1, 1)
+    valid_dt = pd.to_datetime(valid_dt)
+    valid_is = valid_dt.strftime('%Y%m%d_%H%M%S')
+    valid_nx = int((valid_dt - unix_dt).total_seconds())
+
+    start_dt = dt.fromisoformat(start_dt) - td(hours=init_offset)
+    start_is = start_dt.strftime('%Y%m%d_%H%M%S')
+    start_nx = int((start_dt - unix_dt).total_seconds())
+
+    # accumulations are computed from simulation inialization time
+    accu_sec = valid_nx - start_nx
+
+    # Fixes time dim and adds forecast_reference_time var
+    ds_out['time'] = np.array([valid_nx])
+    ds_out['forecast_reference_time'] = \
+            xr.DataArray(np.array([start_nx]), dims=['time'], 
+                    coords=dict(time=ds_out['time']))
+
+    # Declares fill value
+    fill_val = 1e20
+    ds_out.fillna(fill_val)
+
+    # For data vars, who share these attributes
+    repeat_attrs = {
+            'missing_value':fill_val, 
+            'valid_time':valid_is, 
+            'valid_time_ut':valid_nx, 
+            'init_time':start_is, 
+            'init_time_ut':start_nx,
+            'accum_time_sec': accu_sec,
+            }
+
+    # Global DS attributes
+    ds_out.attrs = {
+            'Conventions':'CF-1.6', 
+            'notes':'Created with MET-Tools', 
+            'institution':'CW3E - Scripps Institution of Oceanography',
+            }
+
+    # Global ds dimension and coordinate attributes
+    ds_out.time.attrs = {
+        'long_name': 'Time',
+        'standard_name': 'time',
+        'description': 'Time',
+        'units': 'seconds since 1970-01-01T00:00:00',
+        'calendar': 'standard',
+        'axis': 'T',
+        }
+    
+    ds_out.lat.attrs = {
+        'long_name': 'Latitude',
+        'standard_name': 'latitude',
+        'units': 'degrees_north',
+        'missing_value': fill_val,
+        }
+    
+    ds_out.lon.attrs = {
+        'long_name': 'Longitude',
+        'standard_name': 'longitude',
+        'units': 'degrees_east',
+        'missing_value': fill_val,
+        }
+    
+    ds_out.south_north.attrs = {
+        'long_name': 'y coordinate of projection',
+        'standard_name': 'projection_y_coordinate',
+        'axis': 'Y',
+        'units': 'none',
+        }
+    
+    ds_out.west_east.attrs = {
+        'long_name': 'x coordinate of projection',
+        'standard_name': 'projection_x_coordinate',
+        'axis': 'X',
+        'units': 'none',
+        }
+    
+    ds_out.forecast_reference_time.attrs = {
+        'long_name': 'Forecast Reference Time',
+        'standard_name': 'forecast_reference_time',
+        'valid_time': valid_dt.strftime('%Y%m%d_%H0000'),
+        'init_time': start_dt.strftime('%Y%m%d_%H0000'),
+        'fcst_time': int(accu_sec / 3600),
+        'units': 'seconds since 1970-01-01T00:00:00',
+        'description': 'Simulation initial time',
+        }
+    
+    return ds_out, repeat_attrs
+
+def prep_attrs(ds_in, ds_out, init_offset=0):
+    """
+    Function to assign attributes to new data variables.
+    """
+
+    # Sets up global WRF dataset attributes
+    ds_out, repeat_attrs = global_attrs(ds_in, ds_out, init_offset)
+
+    if 'precip' in ds_out.data_vars:
+        ds_out.precip.attrs = {'description': 'Sum of grid- / convective-scale precipitation',
+                               'standard_name': 'total_precipitation_amount',
+                               'long_name': 'Accumulated Total Precipitation Over Simulation',
+                               'units': 'mm', **repeat_attrs}
+
+    if 'IWV' in ds_out.data_vars:
+        ds_out.IWV.attrs = {'description':'Column-integrated water vapor',
+                            'standard_name':'integrated_water_vapor',
+                            'long_name':'Integrated Water Vapor',
+                            'units':'kg m-2', **repeat_attrs}
+    if 'IVT' in ds_out.data_vars:
+        ds_out.IVT.attrs = {'description':'Column-integrated vapor transport',
+                            'standard_name':'integrated_vapor_transport',
+                            'long_name':'Integrated Vapor Transport',
+                            'units':'kg m-1 s-1', **repeat_attrs}
+    if 'IVTU' in ds_out.data_vars:
+        ds_out.IVTU.attrs = {'description':'Column-integrated vapor transport u-component',
+                             'standard_name':'integrated_vapor_transport_u',
+                             'long_name':'Integrated Vapor Transport U-component',
+                             'units':'kg m-1 s-1', **repeat_attrs}
+    if 'IVTV' in ds_out.data_vars:
+        ds_out.IVTV.attrs = {'description':'Column-integrated vapor transport v-component',
+                             'standard_name':'integrated_vapor_transport_v',
+                             'long_name':'Integrated Vapor Transport V-component',
+                             'units':'kg m-1 s-1', **repeat_attrs}
+
+    return ds_out
+
 def cf_precip(ds_in, init_offset=0):
     """Computes total precip from the dataset in and returns a dataset out.
 
@@ -46,32 +231,15 @@ def cf_precip(ds_in, init_offset=0):
     # unpack grid- / convective-scale precip
     precip_g = ds_in.RAINNC
     precip_c = ds_in.RAINC
-    valid_dt = precip_g.XTIME.values[0]
-    start_dt = ds_in.START_DATE
-
     precip = precip_g + precip_c
 
-    # we write out start / valid times in iso / unix for MET attributes
-    unix_dt = dt(1970, 1, 1)
-    valid_dt = dt.combine(pd.to_datetime(valid_dt).date(), dt.min.time())
-    valid_is = valid_dt.strftime('%Y%m%d_%H%M%S')
-    valid_nx = int((valid_dt - unix_dt).total_seconds())
-
-    start_dt = dt.fromisoformat(start_dt)
-    start_dt = start_dt - td(hours=init_offset)
-    start_is = start_dt.strftime('%Y%m%d_%H%M%S')
-    start_nx = int((start_dt - unix_dt).total_seconds())
-
-    # accumulations are computed from simulation inialization time
-    accu_sec = valid_nx - start_nx
 
     ds_out = xr.Dataset(
             data_vars=dict(
                 precip=(['time', 'south_north', 'west_east'], precip.values),
-                forecast_reference_time=(['time'], np.array([start_nx])),
                 ),
             coords=dict(
-                time=(['time'], np.array([valid_nx])),
+                time=(['time'], np.array([0])),
                 lat=(['south_north', 'west_east'],
                     np.squeeze(precip.XLAT.values)),
                 lon=(['south_north', 'west_east'],
@@ -81,77 +249,17 @@ def cf_precip(ds_in, init_offset=0):
                 ),
             )
 
-    fill_val = 1e20
-    ds_out.fillna(fill_val)
 
-    ds_out.attrs = {'Conventions': 'CF-1.6'}
-
-    ds_out.precip.attrs = {
-        'description': 'Sum of grid- / convective-scale precipitation',
-        'standard_name': 'total_precipitation_amount',
-        'long_name': 'Accumulated Total Precipitation Over Simulation',
-        'units': 'mm',
-        'missing_value': fill_val,
-        'valid_time': valid_is,
-        'valid_time_ut': valid_nx,
-        'init_time': start_is,
-        'init_time_ut': start_nx,
-        'accum_time_sec': accu_sec,
-        }
-
-    ds_out.time.attrs = {
-        'long_name': 'Time',
-        'standard_name': 'time',
-        'description': 'Time',
-        'units': 'seconds since 1970-01-01T00:00:00',
-        'calendar': 'standard',
-        'axis': 'T',
-        }
-
-    ds_out.lat.attrs = {
-        'long_name': 'Latitude',
-        'standard_name': 'latitude',
-        'units': 'degrees_north',
-        'missing_value': fill_val,
-        }
-
-    ds_out.lon.attrs = {
-        'long_name': 'Longitude',
-        'standard_name': 'longitude',
-        'units': 'degrees_east',
-        'missing_value': fill_val,
-        }
-
-    ds_out.south_north.attrs = {
-        'long_name': 'y coordinate of projection',
-        'standard_name': 'projection_y_coordinate',
-        'axis': 'Y',
-        'units': 'none',
-        }
-
-    ds_out.west_east.attrs = {
-        'long_name': 'x coordinate of projection',
-        'standard_name': 'projection_x_coordinate',
-        'axis': 'X',
-        'units': 'none',
-        }
-
-    ds_out.forecast_reference_time.attrs = {
-        'long_name': 'Forecast Reference Time',
-        'standard_name': 'forecast_reference_time',
-        'valid_time': valid_dt.strftime('%Y%m%d_%H0000'),
-        'init_time': start_dt.strftime('%Y%m%d_%H0000'),
-        'fcst_time': int(accu_sec / 3600),
-        'units': 'seconds since 1970-01-01T00:00:00',
-        'description': 'Simulation initial time',
-        }
+    # Assigns attributes
+    ds_out = prep_attrs(ds_in, ds_out, init_offset)
 
     return ds_out
 
 def cf_precip_bkt(ds_curr, ds_prev):
     """Computes accumulated precip between two model times from time series.
 
-    Inputs are precip data arrays at
+    Inputs are precip data arrays at current and previous time over which to
+    compute the accumulation bucket.
     """
     ds_out = ds_curr.rename_vars({'precip': 'precip_bkt'})
     ds_out.precip_bkt.values = ds_curr.precip.values - ds_prev.precip.values
@@ -177,6 +285,63 @@ def cf_precip_bkt(ds_curr, ds_prev):
         'init_time_ut': prev_nx,
         'accum_time_sec': accu_sec,
         }
+
+    return ds_out
+
+def cf_ivt(ds_in):
+    """
+    Function to calculate IVT and IVW and put into cf-compliant xarray dataset.
+    """
+
+    # Unpack IVT variables
+    p_eta = ds_in.P + ds_in.PB
+    p_surf = ds_in.PSFC
+    u_gr, v_gr = unstagger_uv(ds_in.U, ds_in.V)
+    qvapor = ds_in.QVAPOR
+
+    # calculate the pressure at the intersection between eta levels
+    p_int = (p_eta + np.roll(p_eta, 1, axis=1))*0.5
+    p_int[:, 0, :, :] = p_surf
+
+    # Limits IVT calc from surface to >= ~250 mb
+    top_idx = np.min(np.argwhere(p_int.values <= 2500.0)[:, 1])
+    p_int = p_int.isel(bottom_top=slice(0, top_idx))
+    u_gr = u_gr.isel(bottom_top = slice(0, top_idx-1))
+    v_gr = v_gr.isel(bottom_top = slice(0, top_idx-1))
+    qvapor = qvapor.isel(bottom_top = slice(0, top_idx-1))
+
+    # Do calcs
+    r_v_eta = qvapor/(qvapor+1)
+
+    # calculate the difference in pressure between eta levels
+    d_pres = np.diff(p_int, axis=1)
+
+    # calculate the IVT and IWV
+    IWV = ((r_v_eta * d_pres)/9.81).sum(dim='bottom_top')
+    IVTU = ((r_v_eta * d_pres * u_gr)/9.81).sum(dim='bottom_top')
+    IVTV = ((r_v_eta * d_pres * v_gr)/9.81).sum(dim='bottom_top')
+    IVT = np.sqrt((IVTU**2)+(IVTV**2))
+
+    # Prepares output ds
+    ds_out = xr.Dataset(
+            data_vars = dict(
+                IWV=(['time', 'south_north', 'west_east'], IWV.values),
+                IVT=(['time', 'south_north', 'west_east'], IVT.values),
+                IVTU=(['time', 'south_north', 'west_east'], IVTU.values),
+                IVTV=(['time', 'south_north', 'west_east'], IVTV.values)),
+            coords = dict(
+                time = (['time'], np.array([0])),
+                lat = (['south_north', 'west_east'],
+                    np.squeeze(IVT.XLAT.values)),
+                lon = (['south_north', 'west_east'],
+                    np.squeeze(IVT.XLONG.values)),
+                south_north = ('south_north', IVT.south_north.values),
+                west_east = ('west_east', IVT.west_east.values),
+                )
+            )
+
+    # Assigns attributes
+    ds_out = prep_attrs(ds_in, ds_out)
 
     return ds_out
 
